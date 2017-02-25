3ba2a6cdf6773adb98750ed2b7956dfb09eef45a
n.kawajiri <n.kawajiri@livingstyle.co.jp>
2017-02-25 20:36:31 +0900

    UIButtonのlayerを指定するとアニメーションができない

--- CALayer/ViewController.swift
+++ CALayer/ViewController.swift
@@ -12,14 +12,16 @@ import QuartzCore
 class ViewController: UIViewController {
     var image = UIImage()
     var imageLayer = CALayer()
+    var btn = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         image = UIImage.init(named: "type.png")!
-        imageLayer.contents = self.image.cgImage
-        imageLayer.frame = CGRect.init(x: 0, y: 0, width: 200, height: 160)
-        imageLayer.position = CGPoint.init(x: 0, y: 0)
-        self.view.layer.addSublayer(imageLayer)
+        btn.layer.contents = self.image.cgImage
+        btn.layer.frame = CGRect.init(x: 0, y: 0, width: 200, height: 160)
+        btn.layer.position = CGPoint.init(x: 0, y: 0)
+        self.view.addSubview(btn)
+        self.view.layer.addSublayer(btn.layer)
         //moveLayer(imageLayer)
     }
 
@@ -32,12 +34,12 @@ class ViewController: UIViewController {
         CATransaction.begin()
         CATransaction.setAnimationDuration(5.0)
         layer.position = CGPoint.init(x: layer.position.x + 100, y: layer.position.y + 200)
-        layer.opacity = 0
+        //layer.opacity = 0
         CATransaction.commit()
     }
 
     @IBAction func moveBtnTouchUpInside(_ sender: Any) {
-        moveLayer(imageLayer)
+        moveLayer(btn.layer)
     }
 
 }
