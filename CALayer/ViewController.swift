3a5952c212afffe0a1e74475656176299fc72fb7
n.kawajiri <n.kawajiri@livingstyle.co.jp>
2017-02-25 20:16:36 +0900

    Initial Commit

---
+++ CALayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CALayer
+//
+//  Created by netNORIMINGCONCEPTION on 2017/02/25.
+//  Copyright © 2017年 flatLabel56. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
